a7caa2a52841f1b049359c97179e0450c12c63fc
ciccio cappa <cicappa@gmail.com>
2025-03-02 20:56:37 +0100

    new editor

--- editor/editor.js
+++ editor/editor.js
@@ -1,1 +1,14 @@
+document.addEventListener('DOMContentLoaded', async function () {
+    // Misura le dimensioni del viewport
 
+    layout();
+    window.addEventListener('resize', layout);
+});
+
+
+
+function layout() {
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+    
+}
